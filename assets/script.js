a02a955e7366bfedf1e917687bf8353b479f9f82
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-03-11 22:10:29 -0500

    got local storage functions working for two time blocks

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,8 @@ time.text(moment().format("MMMM Do YYYY, h:mm a"));
 var hours = 9
 var currentHour = moment().format("hA");
 
+//=====================================================================================================================
+
 var array0 = [];
 var array1 = [];
 var array2 = [];
@@ -16,6 +18,8 @@ var array5 = [];
 var array6 = [];
 var array7 = [];
 var array8 = [];
+
+//=====================================================================================================================
 
 function createRow() {
 
@@ -55,8 +59,10 @@ function createRow() {
     };
 
 };
+createRow();
 
-// $(".button-0").on("click", grabVal);
+//=====================================================================================================================
+
 
 $(document).on("click", ".button-0", grabVal0);
 
@@ -65,7 +71,18 @@ function grabVal0() {
     array0 = [];
     var value0 = $(".9AM-input").val();
     array0.push(value0);
+    localStorage.setItem("9am", array0);
+    
+    console.log(value0);
 };
+
+
+
+    
+//=====================================================================================================================
+
+
+//=====================================================================================================================
 
 $(document).on("click", ".button-1", grabVal1);
 
@@ -74,7 +91,10 @@ function grabVal1() {
     array1 = [];
     var value1 = $(".10AM-input").val();
     array1.push(value1);
+    localStorage.setItem("10am", array1);
 };
+
+//================================================================================================================
 
 $(document).on("click", ".button-2", grabVal2);
 
@@ -83,7 +103,10 @@ function grabVal2() {
     array2 = [];
     var value2 = $(".11AM-input").val();
     array2.push(value2);
+    localStorage.setItem("11am", array2);
 };
+
+//================================================================================================================
 
 $(document).on("click", ".button-3", grabVal3);
 
@@ -92,7 +115,10 @@ function grabVal3() {
     array3 = [];
     var value3 = $(".12PM-input").val();
     array3.push(value3);
+    localStorage.setItem("12pm", array3);
 };
+
+//================================================================================================================
 
 $(document).on("click", ".button-4", grabVal4);
 
@@ -101,7 +127,10 @@ function grabVal4() {
     array4 = [];
     var value4 = $(".1PM-input").val();
     array4.push(value4);
+    localStorage.setItem("1pm", array4);
 };
+
+//================================================================================================================
 
 $(document).on("click", ".button-5", grabVal5);
 
@@ -110,25 +139,67 @@ function grabVal5() {
     array5 = [];
     var value5 = $(".2PM-input").val();
     array5.push(value5);
+    localStorage.setItem("2pm", array5);
 };
 
-$(document).on("click", ".button-6", grabVal6);
+//================================================================================================================
 
-function grabVal6() {
+$(document).on("click", ".button-6", Val6);
+
+function Val6() {
 
     array6 = [];
     var value6 = $(".3PM-input").val();
     array6.push(value6);
+    storeVal6();
 };
 
-$(document).on("click", ".button-7", grabVal7);
+function storeVal6() {
 
-function grabVal7() {
+    localStorage.setItem("3pm", JSON.stringify(array6));
+};
 
-    array7 = [];
+function grabVal6() {
+    var local6 = JSON.parse(localStorage.getItem("3pm"));
+        array6 = local6;
+    printVal6();
+}
+grabVal6();
+
+function printVal6() {
+    $(".3PM-input").val(array6);
+}
+
+//================================================================================================================
+
+$(document).on("click", ".button-7", Val7);
+
+function Val7() {
+
+    array7 =[];
     var value7 = $(".4PM-input").val();
     array7.push(value7);
+    storeVal7();
 };
+
+function storeVal7() {
+
+    localStorage.setItem("4pm", JSON.stringify(array7));
+};
+
+function grabVal7() {
+    var local7 = JSON.parse(localStorage.getItem("4pm"));
+        array7 = local7;
+    printVal7();
+}
+grabVal7();
+
+function printVal7() {
+    $(".4PM-input").val(array7);
+}
+
+//================================================================================================================
+
 
 $(document).on("click", ".button-8", grabVal8);
 
@@ -137,8 +208,10 @@ function grabVal8() {
     array8 = [];
     var value8 = $(".5PM-input").val();
     array8.push(value8);
+    localStorage.setItem("5pm", array8);
+    console.log();
 };
 
-createRow();
+
 
 
